abf1f30b4286ee271aca17040a7e38d94476ddb6
What Ever <olive.croomes@utexas.edu>
2017-07-05 10:12:42 -0500

    Run files

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+source ./scripts/venv/bin/activate
+
+pip install -r ./scripts/requirements.txt 
+
 python ./src/process_log.py ./log_input/batch_log.json ./log_input/stream_log.json ./log_output/flagged_purchases.json
