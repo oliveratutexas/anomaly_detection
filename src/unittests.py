52d1dd7d211e729b012719f3b0f51083621492fc
What Ever <olive.croomes@utexas.edu>
2017-07-05 09:53:36 -0500

    More unittests

--- src/unittests.py
+++ src/unittests.py
@@ -1,48 +1,48 @@
 import unittest
 # import User, FriendGraph, EventStreamer
-from AnomalyChecker import FriendGraph, User, EventStreamer
-
-class StressTests(unittest.TestCase):
-   def test_large_users(self):
-      pass
-   def test_large_friend_actions(self):
-      pass
-   def test_large_transactions(self):
-      pass
+from datamanager import DataManager
 
 class GraphTests(unittest.TestCase):
 
    def test_add(self):
-      graph = FriendGraph()
-      graph.addFriendship(1,2)
-      graph.addFriendship(1,3)
+      graph = DataManager()
+      graph.addFriendship(1,2,10)
+      graph.addFriendship(1,3,10)
 
       test_dict = dict()
       test_dict[1] = set([2,3])
       test_dict[2] = set([1])
       test_dict[3] = set([1])
       print(graph)
-      self.assertEqual(test_dict,graph.users)
+      self.assertEqual(test_dict,graph.friends)
 
    def test_remove(self):
 
-      graph = FriendGraph()
-      graph.addFriendship(1,2)
-      graph.addFriendship(1,3)
-      graph.removeFriendship(1,2)
+      graph = DataManager()
+      graph.addFriendship(1,2,10)
+      graph.addFriendship(1,3,10)
+      graph.removeFriendship(1,2,10)
 
       test_dict = dict()
       test_dict[1] = set([3])
       test_dict[2] = set([])
       test_dict[3] = set([1])
 
-      self.assertEqual(test_dict,graph.users)
+      self.assertEqual(test_dict,graph.friends)
 
+   @unittest.expectedFailure
    def test_invalid_remove(self):
-      pass
+      graph = DataManager()
+      graph.removeFriendship(1,2,10)
 
-   def test_invalid_add(self):
-      pass
+
+   @unittest.expectedFailure
+   def test_self_add(self):
+      graph = DataManager()
+      graph.addFriendship(1,1,10)
+
+
+
 
 if __name__=='__main__':
    unittest.main()
